53f909064936e51c4345ed6579206a29e3d08417
Abhishek Kumar Rai <rai.abhishek192005@gmail.com>
2023-12-10 09:19:06 +0530

    Adding python file

---
+++ facical tree.py
@@ -0,0 +1,27 @@
+import turtle
+abhi = turtle.Turtle()
+scr=turtle.Screen()
+#putting color
+scr.bgcolor('black')
+abhi.pencolor('white')
+#for more speed and adjusting
+abhi.left(90)
+abhi.speed(0)
+abhi.penup()
+abhi.goto(0,-200)
+abhi.pendown()
+#main 
+def tree(i):
+    if(i<10):
+        return
+    else:
+        abhi.forward(i)
+        abhi.left(30)
+        tree(3*i/4)
+        abhi.right(60)
+        tree(3*i/4)
+        abhi.left(30)
+        abhi.backward(i)        
+        
+tree(100)
+turtle.done()
